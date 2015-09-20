834aa7a52a6510399553d40ee00169c5b97a9e1f
abhikumar90 <abhi.kumar90@hotmail.com>
2015-09-21 02:12:24 +0530

    readme changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'admin@bitlasoft.com',password: 'admin@123',password_confirmation: 'admin@123',
 first_name: 'Admin',last_name: 'bitlasoft',status: true )
+
+User.create(email: 'test1@bitlasoft.com',password: 'test@123',password_confirmation: 'admin@123',
+first_name: 'test1',last_name: 'bitlasoft',status: true )
+
+User.create(email: 'test2@bitlasoft.com',password: 'test@123',password_confirmation: 'admin@123',
+first_name: 'test2',last_name: 'bitlasoft',status: true )
